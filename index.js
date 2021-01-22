1ea454c721da2740c970afe7966854b294e3bafb
Tabatha Bell <tabathambell@live.com>
2021-01-21 22:58:33 -0500

    finished updating employee roles

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+const { exists } = require('fs');
+const { exit } = require('process');
 
 //create the connection to database
 const connection = mysql.createConnection({
@@ -14,15 +16,27 @@ const connection = mysql.createConnection({
 connection.connect(err => {
     if (err) throw err;
     console.log('connected as id ' + connection.threadId);
-    afterConnection();
+    initialPrompt();
 });
 
-afterConnection = () => {
+initialPrompt = () => {
     inquirer
         .prompt(questions)
 
         .then(answers => {
-            console.log(answers);
+            switch (answers.options) {
+                case "View Employees":
+                    viewEmployees()
+                    break;
+                case "Add Employee":
+                    addEmployee()
+                    break;
+                case "Update Employee Role":
+                    aboutRole()
+                    break;
+                default:
+                    exit()
+            };
         })
         .catch(err => {
             if (err) throw err;
@@ -45,58 +59,58 @@ const questions = {
     ],
 };
 
+
 // Update Employee Role
 
-const EmployeeRoleQuestions = [
-    {
-        type: 'choice',
-        name: 'employee',
-        message: 'What employee do you want to update?',
-        choices: '',
-    },
-    {
-        type: 'input',
-        name: 'salary',
-        message: "What is the employee's new role?",
-    },
-];
+function aboutRole() {
+    const query = connection.query('SELECT * FROM employee',
+        function (err, result) {
+            console.log(result);
+            const employeeNames = result.map(employee => ({ name: employee.first_name + " " + employee.last_name, value: employee.id }));
+            const query = connection.query('SELECT * FROM role',
+                function (err, result) {
+                    console.log(result);
+                    const employeeRoles = result.map(role => ({ name: role.title, value: role.id }));
+                    const EmployeeRoleQuestions = [
+                        {
+                            type: 'list',
+                            name: 'employee',
+                            message: 'What employee do you want to update?',
+                            choices: employeeNames,
+                        },
+                        {
+                            type: 'list',
+                            name: 'roles',
+                            message: "What is the employee's new role?",
+                            choices: employeeRoles,
+                        },
+                    ];
+                    inquirer
+                        .prompt(EmployeeRoleQuestions)
 
-inquirer
-    .prompt(EmployeeRoleQuestions)
-  
-    .then(answers => {
-      console.log(answers);
-      updateRole();
-    })
-    .catch(err => {
-        if (err) throw err;
-    });
-
-updateRole = () => {
-    console.log('Updating employee role...\n');
-    const query = connection.query(
-        'UPDATE role SET ?, ?, ? WHERE ?',
-        [
-            {
-                title: answers.title
-            },
-            {
-                salary: answers.salary
-            },
-            {
-                department_id: answers.department_id
-            },
-            {
-                id: answers.id
-            }
-        ],
-        function (err, res) {
-            if (err) throw err;
-            console.log(res.affectedRows + ' record updated!\n');
-            deleteRole();
-        }
-    );
-
-    // logs the actual query being run
-    console.log(query.sql);
+                        .then(answers => {
+                            console.log(answers);
+                            console.log('Updating employee role...\n');
+                            const query = connection.query(
+                                'UPDATE employee SET ? WHERE ?',
+                                [
+                                    {
+                                        role_id: answers.roles
+                                    },
+                                    {
+                                        id: answers.employee
+                                    }
+                                ],
+                                function (err, res) {
+                                    if (err) throw err;
+                                    console.log(res.affectedRows + ' record updated!\n');
+                                    initialPrompt();
+                                }
+                            );
+                        })
+                        .catch(err => {
+                            if (err) throw err;
+                        });
+                })
+        });
 };
